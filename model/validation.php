c1d2b026cafd59df281fb3405fc705472ee68beb
adolfo <agonzalez15@mail.greenriver.edu>
2019-02-02 20:43:52 -0500

    new files

--- model/validation.php
+++ model/validation.php
@@ -1,4 +1,13 @@
 <?php
-//validName()
-//validAge()
-//validPhone()
+/*      Author: Adolfo Gonzalez
+        Date:   2/1/2019
+        File:   validation.php
+
+        Validation Functions for dating website.
+*/
+
+
+//validation functions
+function validName(){};
+function validAge(){};
+function validPhone(){};
