e6f07ea27a65ba51d992c063aa9145f31c65325f
adolfo <agonzalez15@mail.greenriver.edu>
2019-02-03 20:03:07 -0500

    fixed syntax errors

--- model/validation.php
+++ model/validation.php
@@ -7,7 +7,70 @@
 */
 
 
-//validation functions
-function validName(){};
-function validAge(){};
-function validPhone(){};
+/* VALIDATION FUNCTIONS */
+
+/**
+ * This function validates a first name or last name.
+ * @param $name string variable
+ * @return bool true if valid else false
+ */
+function validName($name)
+{
+    if ($name !== '' && ctype_alpha($name)){
+
+        return true;
+
+    }
+    return false;
+}
+
+/**
+ * This function validates that age is not null, greater than 18
+ * and is an integer
+ * @param $age  integer variable
+ * @return bool true if valid else false
+ */
+function validAge($age)
+{
+    if ($age !== '' && is_numeric($age) && $age >= 18 ){
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ * tThis function checks for a valid phone number.
+ * @param $phone integer variable
+ * @return bool true if valid else false
+ */
+function validPhone($phone)
+{
+    if ($phone !== '' && is_numeric($phone)){
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ * Get outdoor activities from array.
+ * @param string $activity
+ * @return bool return true if in array else false
+ */
+function validOutdoors($activity)
+{
+    global $f3;
+    return in_array($activity, $f3->get('outdoors'));
+}
+
+/**
+ * Get indoor activities from array.
+ * @param string $activity
+ * @return bool return true if in array else false
+ */
+function validIndoors($activity)
+{
+    global $f3;
+    return in_array($activity, $f3->get('indoors'));
+}
