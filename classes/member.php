e64e3d1d2d619a675fa7ea44920edbff6bae202c
adolfo <agonzalez15@mail.greenriver.edu>
2019-03-03 04:46:58 -0500

    renamed fname and lname  fields to match database

--- classes/member.php
+++ classes/member.php
@@ -1,184 +1,185 @@
 <?php
-
-/**
- * This is the member class used to create a member object.
- *
- * @author Adolfo Gonzalez
- * @version 1.0
- *
- * File: dating/classes/member.php
- */
-class Member
-{
-    #fields
-    private $_first;
-    private $_last;
-    private $_age;
-    private $_gender;
-    private $_phone;
-    private $_email;
-    private $_state;
-    private $_seeking;
-    private $_bio;
-
-    /**
-     * Member constructor.
-     * @param $first
-     * @param $last
-     * @param $age
-     * @param $gender
-     * @param $phone
-     */
-    public function __construct($first, $last, $age, $gender, $phone)
-    {
-        $this->_first = $first;
-        $this->_last = $last;
-        $this->_age = $age;
-        $this->_gender = $gender;
-        $this->_phone = $phone;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getFirst()
-    {
-        return $this->_first;
-    }
-
-    /**
-     * @param $_first
-     */
-    public function setFirst($_first)
-    {
-        $this->_first = $_first;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLast()
-    {
-        return $this->_last;
-    }
-
-    /**
-     * @param $_last
-     */
-    public function setLast($_last)
-    {
-        $this->_last = $_last;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAge()
-    {
-        return $this->_age;
-    }
-
-    /**
-     * @param $_age
-     */
-    public function setAge($_age)
-    {
-        $this->_age = $_age;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getGender()
-    {
-        return $this->_gender;
-    }
-
-    /**
-     * @param $_gender
-     */
-    public function setGender($_gender)
-    {
-        $this->_gender = $_gender;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhone()
-    {
-        return $this->_phone;
-    }
-
-    /**
-     * @param $_phone
-     */
-    public function setPhone($_phone)
-    {
-        $this->_phone = $_phone;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEmail()
-    {
-        return $this->_email;
-    }
-
-    /**
-     * @param $_email
-     */
-    public function setEmail($_email)
-    {
-        $this->_email = $_email;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getState()
-    {
-        return $this->_state;
-    }
-
-    /**
-     * @param $_state
-     */
-    public function setState($_state)
-    {
-        $this->_state = $_state;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSeeking()
-    {
-        return $this->_seeking;
-    }
-
-    /**
-     * @param $_seeking
-     */
-    public function setSeeking($_seeking)
-    {
-        $this->_seeking = $_seeking;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getBio()
-    {
-        return $this->_bio;
-    }
-
-    /**
-     * @param $_bio
-     */
-    public function setBio($_bio)
-    {
-        $this->_bio = $_bio;
-    }
-}#end of class
+	
+	/**
+	 * This is the member class used to create a member object.
+	 *
+	 * @author  Adolfo Gonzalez
+	 * @version 1.0
+	 *
+	 * File: dating/classes/member.php
+	 */
+	class Member
+		{
+		#fields
+			private $_fname;
+			private $_lname;
+			private $_age;
+			private $_gender;
+			private $_phone;
+			private $_email;
+			private $_state;
+			private $_seeking;
+			private $_bio;
+		
+		/**
+		 * Member constructor.
+		 *
+		 * @param $fname
+		 * @param $lname
+		 * @param $age
+		 * @param $gender
+		 * @param $phone
+		 */
+			public function __construct($fname, $lname, $age, $gender, $phone)
+			{
+				$this -> _fname = $fname;
+				$this -> _lname = $lname;
+				$this -> _age = $age;
+				$this -> _gender = $gender;
+				$this -> _phone = $phone;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getFname()
+			{
+				return $this -> _fname;
+			}
+		
+		/**
+		 * @param $_first
+		 */
+			public function setFname($_fname)
+			{
+				$this -> _fname = $_fname;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getLname()
+			{
+				return $this -> _lname;
+			}
+		
+		/**
+		 * @param $_last
+		 */
+			public function setLname($_lname)
+			{
+				$this -> _lname = $_lname;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getAge()
+			{
+				return $this -> _age;
+			}
+		
+		/**
+		 * @param $_age
+		 */
+			public function setAge($_age)
+			{
+				$this -> _age = $_age;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getGender()
+			{
+				return $this -> _gender;
+			}
+		
+		/**
+		 * @param $_gender
+		 */
+			public function setGender($_gender)
+			{
+				$this -> _gender = $_gender;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getPhone()
+			{
+				return $this -> _phone;
+			}
+		
+		/**
+		 * @param $_phone
+		 */
+			public function setPhone($_phone)
+			{
+				$this -> _phone = $_phone;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getEmail()
+			{
+				return $this -> _email;
+			}
+		
+		/**
+		 * @param $_email
+		 */
+			public function setEmail($_email)
+			{
+				$this -> _email = $_email;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getState()
+			{
+				return $this -> _state;
+			}
+		
+		/**
+		 * @param $_state
+		 */
+			public function setState($_state)
+			{
+				$this -> _state = $_state;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getSeeking()
+			{
+				return $this -> _seeking;
+			}
+		
+		/**
+		 * @param $_seeking
+		 */
+			public function setSeeking($_seeking)
+			{
+				$this -> _seeking = $_seeking;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getBio()
+			{
+				return $this -> _bio;
+			}
+		
+		/**
+		 * @param $_bio
+		 */
+			public function setBio($_bio)
+			{
+				$this -> _bio = $_bio;
+			}
+		}#end of class
--- classes/premiumMember.php
+++ classes/premiumMember.php
@@ -1,210 +1,210 @@
 <?php
-/**
- * This is the premium member class and it extends.
- *
- * @author Adolfo Gonzalez
- * @version 1.0
- *
- * File: dating/classes/premiumMember.php
- */
-
-class PremiumMember extends Member
-{
-    #fields
-    private $_indoorInterests;
-    private $_outdoorInterests;
-
-    /**
-     * PremiumMember constructor.
-     * @param $first
-     * @param $last
-     * @param $age
-     * @param $gender
-     * @param $phone
-     */
-    public function __construct($first, $last, $age, $gender, $phone)
-    {
-        //set parent constructor values
-        parent::__construct($first, $last, $age, $gender, $phone);
-    }
-
-    #Getters and Setters
-
-    /**
-     * @return mixed
-     */
-    public function getIndoorInterests()
-    {
-        return $this->_indoorInterests;
-    }
-
-    /**
-     * @param $indoorInterests
-     */
-    public function setIndoorInterests($indoorInterests)
-    {
-        $this->_indoorInterests = $indoorInterests;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getOutdoorInterests()
-    {
-        return $this->_outdoorInterests;
-    }
-
-    /**
-     * @param $outdoorInterests
-     */
-    public function setOutdoorInterests($outdoorInterests)
-    {
-        $this->_outdoorInterests = $outdoorInterests;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getFirst()
-    {
-        return parent::getFirst();
-    }
-
-    /**
-     * @param $first
-     */
-    public function setFirst($first)
-    {
-        parent:: setFirst($first);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLast()
-    {
-        return parent::getLast();
-    }
-
-    /**
-     * @param $last
-     */
-    public function setLast($last)
-    {
-        parent::setLast($last);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getAge()
-    {
-        return parent::getAge();
-    }
-
-    /**
-     * @param $age
-     */
-    public function setAge($age)
-    {
-        parent::setAge($age);
-    }
-
-    /**
-     *
-     */
-    public function getGender()
-    {
-        parent::getGender();
-    }
-
-    /**
-     * @param $gender
-     */
-    public function setGender($gender)
-    {
-        parent::setGender($gender);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPhone()
-    {
-        return parent::getPhone();
-    }
-
-    /**
-     * @param $phone
-     */
-    public function setPhone($phone)
-    {
-        parent::setPhone($phone);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEmail()
-    {
-        return parent::getEmail();
-    }
-
-    /**
-     * @param $email
-     */
-    public function setEmail($email)
-    {
-        parent::setEmail($email);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getState()
-    {
-        return parent::getState();
-    }
-
-    /**
-     * @param $state
-     */
-    public function setState($state)
-    {
-        parent::setState($state);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSeeking()
-    {
-        return parent::getSeeking();
-    }
-
-    /**
-     * @param $seeking
-     */
-    public function setSeeking($seeking)
-    {
-        parent::setSeeking($seeking);
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getBio()
-    {
-        return parent::getBio();
-    }
-
-    /**
-     * @param $bio
-     */
-    public function setBio($bio)
-    {
-        parent::setBio($bio);
-    }
-
-
-}#end of class
+	/**
+	 * This is the premium member class and it extends.
+	 *
+	 * @author  Adolfo Gonzalez
+	 * @version 1.0
+	 *
+	 * File: dating/classes/premiumMember.php
+	 */
+	
+	class PremiumMember extends Member
+		{
+		#fields
+			private $_indoorInterests;
+			private $_outdoorInterests;
+		
+		/**
+		 * PremiumMember constructor.
+		 *
+		 * @param $fname
+		 * @param $lname
+		 * @param $age
+		 * @param $gender
+		 * @param $phone
+		 */
+			public function __construct($fname, $lname, $age, $gender, $phone)
+			{
+				//set parent constructor values
+				parent ::__construct($fname, $lname, $age, $gender, $phone);
+			}
+		
+		#Getters and Setters
+		
+		/**
+		 * @return mixed
+		 */
+			public function getIndoorInterests()
+			{
+				return $this -> _indoorInterests;
+			}
+		
+		/**
+		 * @param $indoorInterests
+		 */
+			public function setIndoorInterests($indoorInterests)
+			{
+				$this -> _indoorInterests = $indoorInterests;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getOutdoorInterests()
+			{
+				return $this -> _outdoorInterests;
+			}
+		
+		/**
+		 * @param $outdoorInterests
+		 */
+			public function setOutdoorInterests($outdoorInterests)
+			{
+				$this -> _outdoorInterests = $outdoorInterests;
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getFname()
+			{
+				return parent ::getFname();
+			}
+		
+		/**
+		 * @param $first
+		 */
+			public function setFname($fname)
+			{
+				parent :: setFname($fname);
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getLname()
+			{
+				return parent ::getLname();
+			}
+		
+		/**
+		 * @param $last
+		 */
+			public function setLname($lname)
+			{
+				parent ::setLast($lname);
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getAge()
+			{
+				return parent ::getAge();
+			}
+		
+		/**
+		 * @param $age
+		 */
+			public function setAge($age)
+			{
+				parent ::setAge($age);
+			}
+		
+		/**
+		 *
+		 */
+			public function getGender()
+			{
+				parent ::getGender();
+			}
+		
+		/**
+		 * @param $gender
+		 */
+			public function setGender($gender)
+			{
+				parent ::setGender($gender);
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getPhone()
+			{
+				return parent ::getPhone();
+			}
+		
+		/**
+		 * @param $phone
+		 */
+			public function setPhone($phone)
+			{
+				parent ::setPhone($phone);
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getEmail()
+			{
+				return parent ::getEmail();
+			}
+		
+		/**
+		 * @param $email
+		 */
+			public function setEmail($email)
+			{
+				parent ::setEmail($email);
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getState()
+			{
+				return parent ::getState();
+			}
+		
+		/**
+		 * @param $state
+		 */
+			public function setState($state)
+			{
+				parent ::setState($state);
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getSeeking()
+			{
+				return parent ::getSeeking();
+			}
+		
+		/**
+		 * @param $seeking
+		 */
+			public function setSeeking($seeking)
+			{
+				parent ::setSeeking($seeking);
+			}
+		
+		/**
+		 * @return mixed
+		 */
+			public function getBio()
+			{
+				return parent ::getBio();
+			}
+		
+		/**
+		 * @param $bio
+		 */
+			public function setBio($bio)
+			{
+				parent ::setBio($bio);
+			}
+			
+		}#end of class
