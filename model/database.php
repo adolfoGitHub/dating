a005c63f0b635e3d3a763bb829488a1277db2744
adolfo <agonzalez15@mail.greenriver.edu>
2019-03-03 04:53:30 -0500

    fixed spelling

--- model/database.php
+++ model/database.php
@@ -24,144 +24,135 @@
 	require '/home/agonzale/config.php';
 	
 	class Database
-	{
+		{
 		/**
 		 * This function is used to connect to database.
-		 * @return void
+		 *
+		 * @return    return void
 		 */
-		function connect()
-		{
-			try {
-				//Instantiate a database object
-				$dbh = new PDO(DB_DSN, DB_USERNAME,
-					DB_PASSWORD);
-				//echo "Connected to database!!!";
-				return $dbh;
-			} catch(PDOException $e) {
-				echo $e -> getMessage();
-				return;
+			function connect() {
+				try {
+					//Instantiate a database object
+					$dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+					//echo "Connected to database!!!";
+					return $dbh;
+				}
+				catch(PDOException $e) {
+					echo $e -> getMessage();
+					return;
+				}
 			}
-		}
 		
 		/**
 		 * This funciton is used to insert a new mamber to the database.
-		 * @param $fname
-		 * @param $lname
-		 * @param $age
-		 * @param $gender
-		 * @param $phone
-		 * @param $email
-		 * @param $state
-		 * @param $seeking
-		 * @param $bio
-		 * @param $premium
-		 * @param $interests
+		 *
+		 * @param $fname        string first name
+		 * @param $lname        string last name
+		 * @param $age          int age
+		 * @param $gender       string  gender
+		 * @param $phone        int phone
+		 * @param $email        string email
+		 * @param $state        string state
+		 * @param $seeking      string seeking
+		 * @param $bio          string biography
+		 * @param $premium      int 1 for premium 0 for non-premium
+		 * @param $interests    string interests
+		 *
 		 * @return bool
 		 */
-		function insertMember($fname, $lname, $age, $gender, $phone, $email, $state, $seeking,
-							  $bio, $premium, $interests)
-		{
-			global $dbh;
-			//connect to database
-			$dbh -> connect();
-			
-			//1. define the query
-			$sql = "INSERT INTO Members('fname','lname','age','gender','phone','email','state'
+			function insertMember($fname, $lname, $age, $gender, $phone, $email, $state, $seeking, $bio, $premium,
+			                      $interests) {
+				global $dbh;
+				//connect to database
+				$dbh = $this  -> connect();
+				
+				//1. define the query
+				$sql = "INSERT INTO Members('fname','lname','age','gender','phone','email','state'
     							 'seeking','bio', 'premium', 'interests')
             					VALUES (:fname, :lname, :age, :gender, :phone, :email, :state,
             								:seeking, : bio, :premium,:interests)";
-			
-			//2. prepare the statement
-			$statement = $dbh -> prepare($sql);
-			
-			//3. bind parameters
-			$statement -> bindParam(':fname', $fname, PDO::PARAM_STR);
-			$statement -> bindParam(':lname', $lname, PDO::PARAM_STR);
-			$statement -> bindParam(':age', $age, PDO::PARAM_INT);
-			$statement -> bindParam(':gender', $gender, PDO::PARAM_STR);
-			$statement -> bindParam(':phone', $phone, PDO::PARAM_STR);
-			$statement -> bindParam(':email', $email, PDO::PARAM_STR);
-			$statement -> bindParam(':state', $state, PDO::PARAM_STR);
-			$statement -> bindParam(':seeking', $seeking, PDO::PARAM_STR);
-			$statement -> bindParam(':bio', $bio, PDO::PARAM_STR);
-			$statement -> bindParam(':premium', $premium, PDO::PARAM_BOOL);
-			$statement -> bindParam(':interests', $interests, PDO::PARAM_STR);
-			
-			//4. execute the statement
-			$success = $statement -> execute();
-			
-			//5. return the result
-			return $success;
-		}
-		
+				
+				//2. prepare the statement
+				$statement = $dbh -> prepare($sql);
+				
+				//3. bind parameters
+				$statement -> bindParam(':fname', $fname, PDO::PARAM_STR);
+				$statement -> bindParam(':lname', $lname, PDO::PARAM_STR);
+				$statement -> bindParam(':age', $age, PDO::PARAM_INT);
+				$statement -> bindParam(':gender', $gender, PDO::PARAM_STR);
+				$statement -> bindParam(':phone', $phone, PDO::PARAM_STR);
+				$statement -> bindParam(':email', $email, PDO::PARAM_STR);
+				$statement -> bindParam(':state', $state, PDO::PARAM_STR);
+				$statement -> bindParam(':seeking', $seeking, PDO::PARAM_STR);
+				$statement -> bindParam(':bio', $bio, PDO::PARAM_STR);
+				$statement -> bindParam(':premium', $premium, PDO::PARAM_BOOL);
+				$statement -> bindParam(':interests', $interests, PDO::PARAM_STR);
+				
+				//4. execute the statement
+				$success = $statement -> execute();
+				
+				//5. return the result
+				return $success;
+			}
 		
 		/**
 		 * This function gets all  members from the database.
+		 *
 		 * @return array
 		 */
-		function getMembers()
-		{
-			global $dbh;
-			//connect to database
-			$dbh -> connect();
-			
-			//1. define the query
-			$sql = 'SELECT * FROM Members ORDER BY lname';
-			
-			//2. prepare the statement
-			$statement = $dbh -> prepare($sql);
-			
-			//3. bind parameters
-			//--No BINDING--//
-			
-			//4. execute the statement
-			$statement -> execute();
-			
-			//5. return the result
-			$result = $statement -> fetchAll(PDO::FETCH_ASSOC);
-			
-			//print_r($result);
-			return $result;
-		}
+			function getMembers() {
+				global $dbh;
+				//connect to database
+				$dbh = $this -> connect();
+				
+				//1. define the query
+				$sql = 'SELECT * FROM Members';
+				
+				//2. prepare the statement
+				$statement = $dbh -> prepare($sql);
+				
+				//3. bind parameters
+				//--No BINDING--//
+				
+				//4. execute the statement
+				$statement -> execute();
+				
+				//5. return the result
+				$result = $statement -> fetchAll(PDO::FETCH_ASSOC);
+				
+				//print_r($result);
+				return $result;
+			}
 		
 		/**
-		 * This class get  members by id number.
-		 * @param $id
+		 * This class get members by id number.
+		 *
+		 * @param $id    int member id
+		 *
 		 * @return Member|PremiumMember
 		 */
-		function getMember($id)
-		{
-			global $dbh;
-			//connect to database
-			$dbh -> connect();
-			
-			//1. define the query
-			$sql = 'SELECT * FROM  Members WHERE member_id = :id';
-			
-			//2. prepare the statement
-			$statement = $dbh -> prepare($sql);
-			
-			//3. bind parameters
-			$statement -> bindParam(':id', $id, PDO::PARAM_INT);
-			
-			//4. execute the statement
-			$statement -> execute();
-			
-			//5. return the result
-			$result = $statement -> fetch(PDO::FETCH_ASSOC);
-			
-			//if  premium member
-			if($result['premium'] == 1) {
-				$interests = explode(';', $result['interests']);
-				$outdoor = explode(',', $interests[0]);
-				$indoor = explode(',', $interests[1]);
-				return new PremiumMember($result['fname'], $result['lname'], $result['age'],
-					$result['gender'], $result['phone'], $result['email'], $result['state'],
-					$result['seeking'], $result['bio'], $indoor, $outdoor);
+			function getMember($id) {
+				global $dbh;
+				//connect to database
+				$dbh = $this -> connect();
+				
+				//1. define the query
+				$sql = 'SELECT * FROM  Members WHERE member_id = :id';
+				
+				//2. prepare the statement
+				$statement = $dbh -> prepare($sql);
+				
+				//3. bind parameters
+				$statement -> bindParam(':id', $id, PDO::PARAM_INT);
+				
+				//4. execute the statement
+				$statement -> execute();
+				
+				//5. return the result
+				$result = $statement -> fetch(PDO::FETCH_ASSOC);
+				
+					return new Member($result['fname'], $result['last'], $result['age'], $result['gender'],
+						$result['phone'], $result['email'], $result['state'], $result['seeking'], $result['bio'],
+						$result['interests']);
 			}
-			//else return not premium
-			return new Member($result['fname'], $result['lname'], $result['age'],
-				$result['gender'], $result['phone'], $result['email'], $result['state'],
-				$result['seeking'], $result['bio']);
 		}
-	}
--- model/validation.php
+++ model/validation.php
@@ -1,77 +1,84 @@
 <?php
-/*      Author: Adolfo Gonzalez
-        Date:   2/1/2019
-        File:   validation.php
-
-        Validation Functions for dating website.
-*/
-
-
-/*CREATE TABLE Members (member_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, fname VARCHAR(15) NOT NULL,
-                        lname VARCHAR(20) NOT NULL, age INT(2), gender CHAR(1), phone INT(10), email VARCHAR(50),
-                        state CHAR(2), seeking CHAR(1), boi BLOB, premium CHAR(1), image VARCHAR(100),
-                        interests BLOB);
-*/
-
-/* VALIDATION FUNCTIONS */
-
-/**
- * This function validates a first name or last name.
- * @param $name string variable
- * @return bool true if valid else false
- */
-function validName($name)
-{
-    if ($name !== '' && ctype_alpha($name)){
-
-        return true;
-
-    }
-    return false;
-}
-
-/**
- * This function validates that age is not null, greater than 18
- * and is an integer
- * @param $age  integer variable
- * @return bool true if valid else false
- */
-function validAge($age)
-{
-    if ($age !== '' && is_numeric($age) && $age >= 18 ){
-        return true;
-    }
-    return false;
-}
-/**
- * tThis function checks for a valid phone number.
- * @param $phone integer variable
- * @return bool true if valid else false
- */
-function validPhone($phone)
-{
-    if ($phone !== '' && is_numeric($phone)){
-        return true;
-    }
-    return false;
-}
-/**
- * Get outdoor activities from array.
- * @param string $activity
- * @return bool return true if in array else false
- */
-function validOutdoors($activity)
-{
-    global $f3;
-    return in_array($activity, $f3->get('outdoors'));
-}
-/**
- * Get indoor activities from array.
- * @param string $activity
- * @return bool return true if in array else false
- */
-function validIndoors($activity)
-{
-    global $f3;
-    return in_array($activity, $f3->get('indoors'));
-}
+	/*      Author: Adolfo Gonzalez
+			Date:   2/1/2019
+			File:   validation.php
+	
+			VALIDATION FUNCTIONS for dating website.
+	*/
+	/**
+	 * This function validates a first name or last name.
+	 *
+	 * @param $name string variable
+	 *
+	 * @return bool true if valid else false
+	 */
+	function validName($name)
+	{
+		//check not null and alphabegtic
+		if($name !== '' && ctype_alpha($name)) {
+			return TRUE;
+			
+		}
+		return FALSE;
+	}
+	
+	/**
+	 * This function validates that age is not null, greater than 18
+	 * and is an integer
+	 *
+	 * @param $age  integer variable
+	 *
+	 * @return bool true if valid else false
+	 */
+	function validAge($age)
+	{
+		//check valid, numeric and over 18
+		if($age !== '' && is_numeric($age) && $age >= 18) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+	
+	/**
+	 * tThis function checks for a valid phone number.
+	 *
+	 * @param $phone integer variable
+	 *
+	 * @return bool true if valid else false
+	 */
+	function validPhone($phone)
+	{
+		//check not null and is numeric
+		if($phone !== '' && is_numeric($phone)) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+	
+	/**
+	 * Get outdoor activities from array.
+	 *
+	 * @param string $activity
+	 *
+	 * @return bool return true if in array else false
+	 */
+	function validOutdoors($activity)
+	{
+		global $f3;
+		//return outdoor activities array
+		return in_array($activity, $f3 -> get('outdoors'));
+	}
+	
+	/**
+	 * Get indoor activities from array.
+	 *
+	 * @param string $activity
+	 *
+	 * @return bool return true if in array else false
+	 */
+	function validIndoors($activity)
+	{
+		global $f3;
+		//return indoor activities array
+		return in_array($activity, $f3 -> get('indoors'));
+	}
